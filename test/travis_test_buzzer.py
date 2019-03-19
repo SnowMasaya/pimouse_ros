4d53a1a3b510d0fdcd27a9e122489e650d76b163
SnowMasaya <snow.akogi.pgel@gmail.com>
2019-03-19 09:08:34 +0000

    fix test2

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -18,6 +18,7 @@ class BuzzerTest(unittest.TestCase):
 
         with open('/dev/rtbuzzer0', "r") as f:
             data = f.readline()
+            print(data)
             self.assertEqual(data, '1234\n', "value does not writtern to rtbuzzer0")
 
 
